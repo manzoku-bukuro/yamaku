8c63fac8a45ba4cf3fd8ce0646eef0d81cfeec96
manzoku.bukuro <manzoku.bukuro@gmail.com>
2023-07-15 15:39:08 +0900

    homeの不要な要素を削除

--- src/components/Main/Section/Home.tsx
+++ src/components/Main/Section/Home.tsx
@@ -9,13 +9,6 @@ interface SectionProps {
 const Section = () => {
   return (
     <section id="home">
-      <div className="text-center py-40">
-        <h1 className="text-7xl">株式会社　山空</h1>
-        <h2 className="text-3xl mt-8 flex justify-center flex-wrap">
-          <span>空調・換気・衛生設備の</span>
-          <span>トータルエンジニア</span>
-        </h2>
-      </div>
       <div className="concept py-32 text-center">
         <h3 className="main text-5xl">人が繋ぐ未来へ</h3>
         <h4 className="sub text-2xl mt-4 flex justify-center flex-wrap italic">
