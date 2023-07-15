fc159c2688062e0095c47d3a50e286539945573d
manzoku.bukuro <manzoku.bukuro@gmail.com>
2023-07-15 17:07:22 +0900

    titleをclass化

---
+++ src/components/Main/Section/Business.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Section= () => {
+  return (
+    <section id="business">
+      <h2 className="title">事業内容</h2>
+    </section>
+  );
+};
+
+export default Section;
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Section from "./Section";
 import Home from "@/components/Main/Section/Home";
+import Business from "@/components/Main/Section/Business";
 
 const Main = () => {
   return (
     <main>
       <Home />
-      <Section id="business" bg="bg-yellow-500" />
+      <Business />
       <Section id="company" bg="bg-green-500" />
       <Section id="greeting" bg="bg-blue-500" />
       <Section id="access" bg="bg-indigo-500" />
