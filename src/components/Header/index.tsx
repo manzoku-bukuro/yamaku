37045be57c70c8169c12488cc21f1e41b942e55f
manzoku.bukuro <manzoku.bukuro@gmail.com>
2023-07-15 15:38:50 +0900

    ヘッダー導入

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import logo from '@/assets/img/logo.png';
 
 const Header = () => {
   return (
     <header className="fixed top-0 w-full h-20 bg-transparent flex items-center justify-between px-8 z-10">
-      <div>株式会社　山空</div>
+      <img className="w-60" src={logo} alt="株式会社山空" />
       <nav>
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-4 font-bold text-lg ">
           <li><a href="#home">ホーム</a></li>
           <li><a href="#business">事業内容</a></li>
           <li><a href="#company">会社情報</a></li>
