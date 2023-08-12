b5e82ad2b247beff7bd6ddb8eb02a84dda20fe24
manzoku.bukuro <manzoku.bukuro@gmail.com>
2023-08-12 23:29:24 +0900

    会社概要完成

---
+++ src/components/Main/Section/Company.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import '@/assets/scss/company.scss';
+
+const Section= () => {
+  return (
+    <section id="company">
+      <h2 className="title">会社概要</h2>
+      <div className="overview">
+        <table>
+          <tbody>
+            <tr>
+              <th>称号</th>
+              <td>株式会社山空</td>
+            </tr>
+            <tr>
+              <th>所在地</th>
+              <td>〒354-0017　埼玉県富士見市針ケ谷2-19-18</td>
+            </tr>
+            <tr>
+              <th>連絡先</th>
+              <td>
+                <dl>TEL：049-275-7000</dl>
+                <dl>FAX：049-275-7005</dl>
+                <dl>FAX：049-275-7005</dl>
+              </td>
+            </tr>
+            <tr>
+              <th>代表者</th>
+              <td>代表取締役　山崎 一夫</td>
+            </tr>
+            <tr>
+              <th>設立日</th>
+              <td>平成16年4月27日</td>
+            </tr>
+            <tr>
+              <th>資本金</th>
+              <td>500万円</td>
+            </tr>
+            <tr>
+              <th>有資格者数</th>
+              <td>
+                <dl>1級管工事施工管理技士：1名</dl>
+                <dl>1級管工事施工管理技士補：1名</dl>
+                <dl>建築設備士：1名</dl>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default Section;
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Section from "./Section";
 import Home from "@/components/Main/Section/Home";
 import Business from "@/components/Main/Section/Business";
+import Company from "@/components/Main/Section/Company";
 import Greeting from "@/components/Main/Section/Greeting";
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
     <main>
       <Home />
       <Business />
+      <Company />
       <Greeting />
       <Section id="access" bg="bg-indigo-500" />
     </main>
