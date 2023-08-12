adaafb046c5f80de60e33fa42e2a1bfca7662c63
manzoku.bukuro <manzoku.bukuro@gmail.com>
2023-08-12 21:51:36 +0900

    ご挨拶完成

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Section from "./Section";
 import Home from "@/components/Main/Section/Home";
 import Business from "@/components/Main/Section/Business";
+import Greeting from "@/components/Main/Section/Greeting";
 
 const Main = () => {
   return (
     <main>
       <Home />
       <Business />
-      <Section id="company" bg="bg-green-500" />
-      <Section id="greeting" bg="bg-blue-500" />
+      <Greeting />
       <Section id="access" bg="bg-indigo-500" />
     </main>
   );
