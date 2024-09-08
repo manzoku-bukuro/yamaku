790cf44186b968aaf0b1dd03224b222f7aa62290
manzoku-bukuro <manzoku.bukuro@gmail.com>
2024-09-09 01:11:05 +0900

    metaタグ修正

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
           株式会社 山空
         </title>
         <meta name="description" content="埼玉県富士見市にあるエンジニア企業の株式会社山空の公式サイトです。新築工事はもちろんの事、更新工事（リノベーション）においてもオフィス・飲食店・工場・商店など各施設に合わせた最適な空間をお届けいたします" />
-        <meta name="google-site-verification" content="5WNc2EJRhCKsRGBNjhtG55Db7ZXTaMdBgaYHVpXAFic" />
+        <meta name="google-site-verification" content="VEy1uD32BYr6T3hQbYbcBuNqghc2CoE51wkYNP-8dzg" />
       </Helmet>
       <Header />
       <Main />
