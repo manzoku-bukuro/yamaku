38561f8873fd8cf8b341276aaf9abe8c4acfd261
manzoku-bukuro <manzoku.bukuro@gmail.com>
2024-09-22 18:01:09 +0900

    ヘッダークリックの仕組みを修正

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,6 +9,10 @@ const Header = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <header className={`fixed top-0 w-full md:h-20 h-10 ${isOpen ? 'bg-white md:bg-transparent' : 'bg-transparent'} flex items-center justify-between px-8 z-10`}>
       <span className="md:text-3xl text-xl">株式会社 山 空</span>
@@ -17,11 +21,11 @@ const Header = () => {
       </button>
       <nav className={`${isOpen ? 'block' : 'hidden'} md:block absolute md:static top-full left-0 w-full md:w-auto bg-white md:bg-transparent`}>
         <ul className="flex flex-col md:flex-row font-bold md:text-lg text-sm">
-          <li className="border-b md:border-none"><a href="#home" className="block py-2 px-4">ホーム</a></li>
-          <li className="border-b md:border-none"><a href="#business" className="block py-2 px-4">事業内容</a></li>
-          <li className="border-b md:border-none"><a href="#company" className="block py-2 px-4">会社情報</a></li>
-          <li className="border-b md:border-none"><a href="#greeting" className="block py-2 px-4">ごあいさつ</a></li>
-          <li className="border-b md:border-none"><a href="#access" className="block py-2 px-4">アクセス</a></li>
+          <li className="border-b md:border-none"><a href="#home" className="block py-2 px-4" onClick={handleLinkClick}>ホーム</a></li>
+          <li className="border-b md:border-none"><a href="#business" className="block py-2 px-4" onClick={handleLinkClick}>事業内容</a></li>
+          <li className="border-b md:border-none"><a href="#company" className="block py-2 px-4" onClick={handleLinkClick}>会社情報</a></li>
+          <li className="border-b md:border-none"><a href="#greeting" className="block py-2 px-4" onClick={handleLinkClick}>ごあいさつ</a></li>
+          <li className="border-b md:border-none"><a href="#access" className="block py-2 px-4" onClick={handleLinkClick}>アクセス</a></li>
         </ul>
       </nav>
     </header>
